931117cf0e4414019d1eb60968e12a9db884e1b1
mohamadalabsi <absimoda1@gmail.com>
2025-05-02 21:29:30 +0200

    some notes before starting with graphQl

--- src/main/java/org/me/learning/graphql/controller/PlayerController.java
+++ src/main/java/org/me/learning/graphql/controller/PlayerController.java
@@ -1,8 +1,56 @@
 package org.me.learning.graphql.controller;
 
 
-import org.springframework.web.bind.annotation.RestController;
+import org.me.learning.graphql.model.Player;
+import org.me.learning.graphql.service.PlayerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class PlayerController {
+
+    @Autowired
+    PlayerService playerService ;
+
+    @GetMapping("player")
+    public List<Player> findAllPlayers(){
+        return playerService.findAllPlayers();
+    }
+
+    @PutMapping("player")
+    public Player updatePlayer (@RequestBody Player player){
+      return  playerService.updatePlayer(player);
+
+    }
+
+    @PostMapping("player")
+    public Player addPlayer (@RequestBody Player player){
+        return  playerService.createPlayer(player);
+
+//        Player player1 = playerService.createPlayer(player);
+//          return player1.getId();
+//         now if we have lets say a backend for a big project and this backend connected to android , apple and web application
+//        like this example when i want to return players it will return player (all attributes ), and the filter it and display ,  what we want  what if i want for android just the name and image and not the id
+//        i have to make other method for just android , cuz here it does not work in the same method and sometimes more than one method if i want to return specific thing
+//        so we use graph ql for this , with it we can return specific things for each client
+        
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
 }
--- src/main/java/org/me/learning/graphql/service/PlayerService.java
+++ src/main/java/org/me/learning/graphql/service/PlayerService.java
@@ -2,9 +2,11 @@ package org.me.learning.graphql.service;
 
 
 import org.me.learning.graphql.model.Player;
+import org.me.learning.graphql.model.Team;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -12,22 +14,64 @@ import java.util.concurrent.atomic.AtomicInteger;
 @Service
 public class PlayerService {
 
-    ArrayList <Player> players = new ArrayList<>();
-//     this to generate ids
-    AtomicInteger id = new AtomicInteger(0);
+    ArrayList<Player> players = new ArrayList(Arrays.asList(new Player(1, "mohammad alabsi ", Team.BARCELONA)));
+    //     this to generate ids
+//    AtomicInteger id = new AtomicInteger(0);
 
 
-    public List<Player> findAllPlayers (){
-        return  players;
+    public List<Player> findAllPlayers() {
+        System.out.println(players);
+        return players;
+
     }
-    public Optional<Player> findOne (Integer id ){
-        return  players.stream().filter(player ->  player.getId() == id).findFirst();
+
+    public Optional<Player> findOne(Integer id) {
+        return players.stream().filter(player -> player.getId() == id).findFirst();
+//         .or else return neu player , but without optional
     }
 
 
-    public Player createPlayer(Player player){
-        Player newPlayer = new Player( id.incrementAndGet() , player.getName(), player.getTeam());
-        players.add(newPlayer);
-        return  newPlayer;
+    public Player createPlayer(Player player) {
+//        Player newPlayer = new Player(player.getId(), player.getName(), player.getTeam());
+//        players.add(newPlayer);  this works if you have the all player attributes desperately and not object player
+        players.add(player);
+        return player;
     }
+
+    public Player deletePlayer(int id) {
+        Player player1 = players.stream().filter(player -> player.getId() == id).findFirst().orElseThrow(() -> new IllegalArgumentException());
+//       here optional or/and   throw exception
+        players.remove(player1);
+        return player1;
+
+    }
+
+    public Player updatePlayer(Player player) {
+//       Player newPlayer = new Player( player.getId() , player.getName(), player.getTeam());
+      Optional <Player> player1 = players.stream().filter(c ->  c.getId() ==player.getId()).findFirst();
+      System.out.println(player1);
+      if (player1.isPresent()){
+          Player player2= player1.get(); // this is optional method , cuz we have optional<player> and not list or player
+
+          System.out.println(player2);
+          int index= players.indexOf(player2);
+          return  players.set(index, player);
+      }else{
+           throw new IllegalArgumentException();
+      }
+
+//        int index = 0;
+//        for (int i = 0; i < players.size(); i++) {
+//            if (players.get(i).getId()== player.getId()) {
+//                index = i;
+//            }
+//
+//
+//        }
+//         players.set(index, player);
+//        return  player;
+
+
+    }
+
 }
